e8a442faa51ab9ddd2d1a27986f51b02d5742540
GeekAppleMan <Jaimehbo13@hotmail.com>
2021-12-06 12:28:12 -0700

    add

---
+++ Almacen_Jane_Software/NewFolder1/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Almacen_Jane_Software.NewFolder1
+{
+    class Class1
+    {
+    }
+}
